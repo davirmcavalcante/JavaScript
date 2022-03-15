672192985461f29de80b4193709bb43cdf706119
Davi Ricardo <99710407+davirmcavalcante@users.noreply.github.com>
2022-03-15 00:39:19 -0300

    Novos Exercicios

---
+++ Exercicios/Ex5/ex5.1.js
@@ -0,0 +1,28 @@
+var vel = 70
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if(vel > 60){ // Condição Simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+var pais = 'Brasil'
+console.log(`Vivendo em ${pais}`)
+if(pais == 'Brasil'){
+    console.log(`Você é Brasileiro!`)
+} else{
+    console.log(`Você é Estrangeiro!`)
+}
+
+/* Condicional if / if-else:
+- Condição Simples:
+if(condição){
+    bloco de comandos
+}
+
+- Condição Composta:
+if(condição){
+    bloco de comandos 1
+} else{
+    bloco de comandos 2
+}
+*/
