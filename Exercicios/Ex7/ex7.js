3dc5a076648492e9f13efa42b7a7a65e4d0eeea3
Davi Ricardo <99710407+davirmcavalcante@users.noreply.github.com>
2022-03-17 01:44:09 -0300

    Novos Exercicios

---
+++ Exercicios/Ex7/ex7.js
@@ -0,0 +1,33 @@
+var c = 1
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+c = 5
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/* 
+O while é uma estrutura de repetição com teste lógico no início e depois executa o bloco de comandos.
+while (condição){
+    bloco de comandos
+}
+
+O do while é uma estrutura de repetiçao que executa o bloco de comandos primeiro e depois faz o teste lógico.
+do{
+    bloco de comandos
+} while (condição)
+
+Note que o teste lógico gerando a repetição ocorre até que o valor do teste seja falso.
+*/
+/*
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+console.log('Passo')
+*/
---
+++ Exercicios/Ex8/ex8.js
@@ -0,0 +1,21 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+//-------------------------
+console.log('Inicio')
+
+for (var c = 1; c <= 5; c++){
+    console.log(c)
+}
+
+console.log('Fim')
+/*
+for (inicialização; teste lógico; incremento){
+    bloco de comandos
+}
+*/
